2c9df1ee289e38d2454d2736e0cd7fbcc54e596b
kanhaiyakumartech <kanhaiyakumar@38937@gmail.com>
2023-11-18 18:57:25 +0530

    map and  UD function

---
+++ src/Python_Practice-18/Map.py
@@ -0,0 +1,8 @@
+
+def addition_of_Number(n):
+	return n + n
+
+#  here i take a number in map and Now we gate double all numbers using map()
+numbers = (8, 6, 3, 9)
+My_result = map(addition_of_Number, numbers)
+print(list(My_result))
