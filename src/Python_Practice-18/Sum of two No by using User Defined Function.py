4f58b4bf2d0a5f171451dfa6f663298f49a31d9e
kanhaiyakumartech <kanhaiyakumar@38937@gmail.com>
2023-11-18 11:30:56 +0530

    practice

---
+++ src/Python_Practice-18/Sum of two No by using User Defined Function.py
@@ -0,0 +1,11 @@
+
+# Sum of two number with the help of  user-defined functions
+
+def Sum_of_Two_No (x,y):
+   sum = x + y
+   return sum
+
+num1 = 5
+num2 = 6
+
+print("Total Sum of Two no = ",Sum_of_Two_No (num1, num2))
